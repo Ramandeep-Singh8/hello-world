cdc4cc81da15c7ab5a5ce2051f0d0a0303fd9f9e
Ramandeep Singh <raman.s@vuedata.in>
2021-12-15 16:20:26 +0530

    form

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Formpage from './form';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Formpage />
   </React.StrictMode>,
   document.getElementById('root')
 );
