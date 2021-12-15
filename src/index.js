e3babdb08935a179d6e667eaef61553a3fec018d
Ramandeep Singh <raman.s@vuedata.in>
2021-12-15 15:33:39 +0530

    Update Component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import BirthdayCounter from './BirthdayCounter';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BirthdayCounter />
   </React.StrictMode>,
   document.getElementById('root')
 );
